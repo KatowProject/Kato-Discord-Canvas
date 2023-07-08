072cf597bbdb98e088d704cdbc050d00c235fe53
KatowProject <naufalfaqih443@gmail.com>
2023-07-08 21:53:04 +0700

    partial update

--- test/test-leaderboard.js
+++ test/test-leaderboard.js
@@ -7,18 +7,18 @@ try {
 
     const donatur = [
         {
-            username: 'Rizky',
-            avatar: 'https://cdn.discordapp.com/avatars/458342161474387999/22926c692b8669497cee8836d8ba530d.png?size=4096',
+            username: 'User 1',
+            avatar: 'https://cdn.discordapp.com/avatars/725735701739601980/12a8a5383ba3b2dca70a4dc3773bf072.png?size=4096',
             donation: 'Rp1.000.000'
         },
         {
-            username: 'Atmaja',
+            username: 'User 2',
             avatar: 'https://cdn.discordapp.com/avatars/458342161474387999/22926c692b8669497cee8836d8ba530d.png?size=4096',
             donation: 'Rp500.000'
         },
         {
-            username: 'Rizky Atmaja',
-            avatar: 'https://cdn.discordapp.com/avatars/458342161474387999/22926c692b8669497cee8836d8ba530d.png?size=4096',
+            username: 'User 3',
+            avatar: 'https://cdn.discordapp.com/avatars/750234101121810435/102adef0261242515b79a618c1111905.png?size=4096',
             donation: 'Rp250.000'
         }
     ]
